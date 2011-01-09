347a41811ffe3aa210267b0059400660e6b3b6a1
Endre Oma <eoma@kvarteret.no>
2011-01-10 00:45:58 +0100

    Bugfix: template dakLocationAdmin/_list_td_tabular used wrong variable name and route name, again

--- modules/dakLocationAdmin/templates/_list_td_tabular.php
+++ modules/dakLocationAdmin/templates/_list_td_tabular.php
@@ -1,5 +1,5 @@
 <td class="sf_admin_text sf_admin_list_td_name">
   <span style="<?php echo 'margin-left: ' . ($location['level'] * 1.3) . 'em' ?>">
-    <?php echo link_to($dak_location_admin->getName(), 'dak_location_admin_edit', $location) ?>
+    <?php echo link_to($dak_location->getName(), 'dak_location_admin_edit', $location) ?>
   </span>
 </td>
