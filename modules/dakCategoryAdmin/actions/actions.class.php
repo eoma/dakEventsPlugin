71d71a01ac5f63188fd3048c88884498f3c05f00
Endre Oma <eoma@kvarteret.no>
2011-01-10 00:31:13 +0100

    Bugfix: class dakCategoryAdminActions extended wrong non-existing class

--- modules/dakCategoryAdmin/actions/actions.class.php
+++ modules/dakCategoryAdmin/actions/actions.class.php
@@ -11,6 +11,6 @@ require_once dirname(__FILE__).'/../lib/dakCategoryAdminGeneratorHelper.class.ph
  * @author     Endre Oma
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class dakCategoryAdminActions extends autoCategoryActions
+class dakCategoryAdminActions extends autodakCategoryAdminActions
 {
 }
