095e1b0f27a23c3438174307550a576ee0697e0c
Endre Oma <eoma@kvarteret.no>
2011-01-10 16:26:51 +0100

    Bugfix: use correct variable name for dak_event in partial dakEventAdmin/eventHasFestival

--- modules/dakEventAdmin/templates/_eventHasFestival.php
+++ modules/dakEventAdmin/templates/_eventHasFestival.php
@@ -1,5 +1,5 @@
 <?php
 use_helper('Date');
-if ($event->relatedExists('festival')) {
-  echo 'Part of ' . link_to($event->getFestival()->getTitle() . ' ' . format_date($event->getFestival()->getStartDate()), '@dak_festival_admin_show?id=' . $event->getFestival()->getId()) . '.<br />';
+if ($dak_event->relatedExists('festival')) {
+  echo 'Part of ' . link_to($dak_event->getFestival()->getTitle() . ' ' . format_date($dak_event->getFestival()->getStartDate()), '@dak_festival_admin_show?id=' . $dak_event->getFestival()->getId()) . '.<br />';
 }
