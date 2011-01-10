6a821ec3b6cfcf44eeca5a20377891f734737d16
Endre Oma <eoma@kvarteret.no>
2011-01-10 18:39:08 +0100

    Add final css for the green button for the new action

--- data/generator/sfDoctrineModule/dakAdmin/template/templates/_assets.php
+++ data/generator/sfDoctrineModule/dakAdmin/template/templates/_assets.php
@@ -1,8 +1,10 @@
+
 <?php if (isset($this->params['css']) && ($this->params['css'] !== false)): ?> 
 [?php use_stylesheet('<?php echo $this->params['css'] ?>', 'first') ?] 
 <?php elseif(!isset($this->params['css'])): ?> 
 [?php use_stylesheet('<?php echo sfConfig::get('sf_admin_module_web_dir').'/css/global.css' ?>', 'first') ?] 
 [?php use_stylesheet('<?php echo sfConfig::get('sf_admin_module_web_dir').'/css/default.css' ?>', 'first') ?] 
 <?php endif; ?>
-[?php use_stylesheet('<?php echo sfConfig::get('dak_admin_module_web_dir').'/css/admin.css' ?>') ?]
+
+[?php use_stylesheet('<?php echo sfConfig::get('dak_admin_module_web_dir').'/css/admin.css' ?>', 'first') ?]
 [?php use_javascript('<?php echo sfConfig::get('dak_admin_module_web_dir').'/js/adminTools.js' ?>') ?]
