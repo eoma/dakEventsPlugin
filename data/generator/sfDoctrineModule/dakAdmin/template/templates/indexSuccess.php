2c3dc36df99b6f6689efde8d413cbce088595b7e
Endre Oma <eoma@kvarteret.no>
2011-01-10 17:05:53 +0100

    Add dakAdmin/list_batch_actions and dakAdmin/list_actions to the top of the list

--- data/generator/sfDoctrineModule/dakAdmin/template/templates/indexSuccess.php
+++ data/generator/sfDoctrineModule/dakAdmin/template/templates/indexSuccess.php
@@ -20,6 +20,10 @@
 <?php if ($this->configuration->getValue('list.batch_actions')): ?>
     <form action="[?php echo url_for('<?php echo $this->getUrlForAction('collection') ?>', array('action' => 'batch')) ?]" method="post">
 <?php endif; ?>
+    <ul class="sf_admin_actions">
+      [?php include_partial('<?php echo $this->getModuleName() ?>/list_batch_actions', array('helper' => $helper)) ?]
+      [?php include_partial('<?php echo $this->getModuleName() ?>/list_actions', array('helper' => $helper)) ?]
+    </ul>
     [?php include_partial('<?php echo $this->getModuleName() ?>/list', array('pager' => $pager, 'sort' => $sort, 'helper' => $helper)) ?]
     <ul class="sf_admin_actions">
       [?php include_partial('<?php echo $this->getModuleName() ?>/list_batch_actions', array('helper' => $helper)) ?]
