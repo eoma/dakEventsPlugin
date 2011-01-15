6a4284be7e0760450d7de3860bdd235f9299b2d7
Endre Oma <eoma@kvarteret.no>
2011-01-15 20:45:47 +0100

    Add partial dakFestival/showFullFestival

---
+++ modules/dakFestival/templates/_showFullFestival.php
@@ -0,0 +1,75 @@
+<?php 
+use_helper('I18N', 'Date');
+
+// rp is an acronym for route prefix
+$rp = array(
+  'event'    => '@dak_event',
+  'festival' => '@dak_festival',
+  'location' => '@dak_location',
+  'arranger' => '@dak_arranger',
+  'category' => '@dak_category',
+);
+
+if (isset($inAdmin) && ($inAdmin == true)) {
+  foreach ($rp as &$v) $v .= '_admin';
+} else {
+  $inAdmin = false;
+}
+
+?>
+
+<h1><?php echo $festival['title'] ?></h1>
+
+<div id="eventData">
+  <p>
+    <b><?php echo __('Where?') ?></b> 
+    <?php 
+      if (!$festival['location_id']) {
+        echo $festival['customLocation']; 
+      } else {
+        echo link_to($festival['commonLocation']['name'], $rp['location'] . '_show?id='. $festival['location_id']);
+      } 
+     ?>
+    <br />
+    <b><?php echo __('When?') ?></b> 
+    <?php 
+    if ($festival['startDate'] == $festival['endDate']):
+    ?>
+    <?php echo __('%1% from %2% to %3%',  array('%1%' => format_date($festival['startDate']), '%2%' => $festival['startTime'], '%3%' => $festival['endTime'])) ?>
+    <?php else: ?>
+    <?php echo __('from %1% to %2%', array('%1%' => format_date($festival['startDate']) . ' ' . $festival['startTime'], '%2%' => format_date($festival['endDate']) . ' ' . $festival['endTime'])) ?>
+    <?php endif ?>
+    <br />
+    <b><?php echo __('Who?') ?></b>
+    <?php
+    if (count($festival['arrangers']) > 0) {
+      foreach ($festival['arrangers'] as $arranger) {
+        echo link_to($arranger['name'], $rp['arranger'] . '_show?id=' . $arranger['id']) . " \n";
+      }
+    }
+    ?>
+    <br />
+    <small><?php echo __('Created at %1%. Updated at %2%.', array('%1%' => format_datetime($festival['created_at']), '%2%' => format_datetime($festival['updated_at']))) ?></small>
+  </p>
+</div>
+
+<div id="eventContent">
+
+ <p><?echo $festival->getRaw('leadParagraph'); ?></p>
+ <?echo $festival->getRaw('description'); ?>
+
+  <?php if (!empty($festival['linkout'])): ?>
+  <p><?php echo __('Read more <a href="%1%">here</a>', array('%1%' => $festival['linkout'])) ?></p>
+  <?php endif ?>
+
+  <h2><?php echo __('Events at this festival') ?></h2>
+
+  <?php include_partial('dakEvent/listShortDescription', array('events' => $pager->getResults(), 'inAdmin' => $inAdmin)) ?>
+
+  <?php if ($pager->haveToPaginate()): ?>
+    <?php include_partial('global/pager', array('route' => $rp['festival'] . '_show?id=' . $festival['id'], 'pager' => $pager, 'inAdmin' => $inAdmin)) ?>
+  <?php endif ?>
+</div>
+
+
+
