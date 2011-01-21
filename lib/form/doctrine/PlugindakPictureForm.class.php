f56ff9238d0a4c0d4e1771346675cdad403cd82a
Endre Oma <eoma@kvarteret.no>
2011-01-21 15:04:52 +0100

    Set custom file widget for picture

--- lib/form/doctrine/PlugindakPictureForm.class.php
+++ lib/form/doctrine/PlugindakPictureForm.class.php
@@ -22,5 +22,14 @@ class PlugindakPictureForm extends BasedakPictureForm
     $this->validatorSchema['filename']->setOption('mime_types', 'web_images');
     $this->validatorSchema['filename']->setOption('path', sfConfig::get('sf_upload_dir') . '/pictures');
 
+    sfApplicationConfiguration::getActive()->loadHelpers(array('Url'));
+
+    $this->setWidget('filename', new sfWidgetFormInputFileEditable(array(
+      'file_src'    => public_path('') . basename(sfConfig::get('sf_upload_dir')) . '/pictures/'.$this->getObject()->getFilename(),
+      'edit_mode'   => !$this->isNew(),
+      'is_image'    => true,
+      'with_delete' => false,
+    )));
+
   }
 }
