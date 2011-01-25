7bdbe2d6231162ad33483496f496dc359590fd4a
eoma <endre.88.oma@gmail.com>
2011-01-26 00:21:22 +0100

    make partial dakGlobal/picture use ImageHelper

--- modules/dakGlobal/templates/_picture.php
+++ modules/dakGlobal/templates/_picture.php
@@ -1,15 +1,20 @@
 <?php
+use_helper('Image');
+
 if (!isset($format)) {
   $format = 'list';
 }
+
+$imgOptions = array(
+  'alt' => $picture['description'],
+);
+
+$imgOptions += ImageHelper::TransformSize($format, $picture['width'], $picture['height']);
+
 $picArgs = array(
   'format' => $format,
   'type' => 'dakPicture',
   'id' => $picture['id'],
 );
 
-echo image_tag(url_for('dak_thumb', $picArgs),
-  array(
-    'alt' => $picture['description'],
-  )
-);
+echo image_tag(url_for('dak_thumb', $picArgs), $imgOptions);
