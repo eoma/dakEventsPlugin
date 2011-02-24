06258450ba4e7ebda6840a1997dcc0674d73de65
Endre Oma <eoma@kvarteret.no>
2011-02-24 14:02:58 +0100

    Try to detect extension from mimetype in template dakGlobal/picture

--- modules/dakGlobal/templates/_picture.php
+++ modules/dakGlobal/templates/_picture.php
@@ -17,4 +17,10 @@ $picArgs = array(
   'id' => $picture['id'],
 );
 
+$extension = substr($picture['mime_type'], 6);
+
+if (in_array($extension, array('png', 'gif', 'jpg', 'jpeg'))) {
+  $picArgs['sf_format'] = $extension;
+}
+
 echo image_tag(UrlExtraHelper::url_for_app('frontend', 'dak_thumb', $picArgs), $imgOptions);
