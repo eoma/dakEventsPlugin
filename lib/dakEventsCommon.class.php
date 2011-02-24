498502c8b9087173888a0b2be4f45ba4de4b5cab
Endre Oma <eoma@kvarteret.no>
2011-02-24 13:52:55 +0100

    Add toolbar setup for limited block (one paragraph) html

--- lib/dakEventsCommon.class.php
+++ lib/dakEventsCommon.class.php
@@ -8,6 +8,11 @@ class dakEventsCommon
     return array(array('Source', 'RemoveFormat', '-', 'Copy', 'Cut', 'Paste', 'PasteText', 'PasteFromWord'));
   }
 
+  public static function CKEditorToolbarBlock ()
+  {
+    return array(array('Source', 'RemoveFormat', '-', 'Copy', 'Cut', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Bold', 'Italic', '-', 'Link', 'Unlink'));
+  }
+
   public static function CKEditorToolbarCommon ()
   {
     return array(array('Source', 'RemoveFormat', '-', 'Copy', 'Cut', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Bold', 'Italic', 'Underline', 'Strike', '-', 'NumberedList','BulletedList','-','Outdent','Indent','Blockquote', '-', 'Image', 'Link', 'Unlink'));
