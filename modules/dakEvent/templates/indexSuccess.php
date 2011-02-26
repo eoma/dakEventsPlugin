5b528ab5decfe20ea2fa77883eb307d5e0c17d1c
Endre Oma <eoma@kvarteret.no>
2011-02-26 15:52:02 +0100

    Add link to ical upcoming events in dakEvent index

--- modules/dakEvent/templates/indexSuccess.php
+++ modules/dakEvent/templates/indexSuccess.php
@@ -2,6 +2,12 @@
 <?php slot('title', __('Event list')) ?>
 <h1><?php echo __('Event list') ?></h1>
 
+<p>
+  <a href="<?php echo url_for('@dak_api_upcomingEvents?sf_format=ical') ?>">
+    <?php echo __('Add upcoming events to your calendar (iCalendar format)') ?>
+  </a>
+</p>
+
 <?php include_partial('listShortDescription', array('events' => $pager->getResults())) ?>
 
 <?php if ($pager->haveToPaginate()): ?>
