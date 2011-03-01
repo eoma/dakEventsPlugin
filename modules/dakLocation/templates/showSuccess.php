15f3015542df9d5d6fe6e65ec767f444b58e00cc
Endre Oma <eoma@kvarteret.no>
2011-03-01 13:46:34 +0100

    Use calendar view in dakLocation

--- modules/dakLocation/templates/showSuccess.php
+++ modules/dakLocation/templates/showSuccess.php
@@ -12,8 +12,18 @@
 
 <h2><?php echo __('Events scheduled for %1%', array('%1%' => $location['name'])) ?></h2>
 
-<?php include_partial('dakEvent/listShortDescription', array('events' => $pager->getResults())) ?>
+<p>
+  <label><input type="radio" name="menu" checked="checked" onclick="$('#itemView').show();$('#calendarView').hide()" />Show in item view</label>
+  <label><input type="radio" name="menu" onclick="$('#itemView').hide();$('#calendarView').show()" />Show in calendar view</label>
+</p>
 
-<?php if ($pager->haveToPaginate()): ?>
+<div id="itemView">
+<?php  include_partial('dakEvent/listShortDescription', array('events' => $pager->getResults())) ?>
+
+<?php  if ($pager->haveToPaginate()): ?>
   <?php include_partial('dakGlobal/pager', array('route' => '@dak_location_show?id=' . $location['id'], 'pager' => $pager)) ?>
 <?php endif ?>
+</div>
+<div id="calendarView">
+  <?php include_partial('dakGlobal/fullCalendar', array('url' => '@dak_api_filteredEvents', 'extraUrlQuery' => 'location_id=' . $location['id'])) ?>
+</div>
