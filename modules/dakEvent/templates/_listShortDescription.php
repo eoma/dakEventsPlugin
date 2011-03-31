a04ec67ca4e3baa01a18d33ed79a934a007427e8
Endre Oma <eoma@kvarteret.no>
2011-03-31 11:16:02 +0200

    Float images in event list to the left

--- modules/dakEvent/templates/_listShortDescription.php
+++ modules/dakEvent/templates/_listShortDescription.php
@@ -28,27 +28,29 @@ if (isset($inAdmin) && ($inAdmin == true)) {
         <?php if($event['primaryPicture_id'] > 0) 
           include_partial('dakGlobal/picture', array('picture' => $event['primaryPicture'], 'format' => 'list'));
         ?>
-        <?php echo link_to($event['title'], $rp['event'] . '_show?id=' . $event['id']) ?><br />
-        <span>
-          <?php
-          if (strlen(strip_tags($event->getRaw('leadParagraph'))) > 100) { 
-            echo substr(strip_tags($event->getRaw('leadParagraph')), 0, 97) . '...';
-          } else {
-            echo strip_tags($event->getRaw('leadParagraph'));
-          }
-          ?>
-        </span><br />
-        <?php echo __('When?') ?> <?php include_partial('dakEvent/startEndDateTime', array('event' => $event)) ?><br />
-        <?php echo __('Location') ?>: <?php include_partial('dakEvent/location', array('event' => $event)) ?><br />
-        <?php if (strlen($event['covercharge']) > 0): ?>
-          <b><?php echo __('Covercharge') ?></b>: <?php echo $event['covercharge'] ?><br />
-        <?php endif ?>
-        <?php echo __('Arranger') ?>: <?php echo link_to($event['arranger']['name'], $rp['arranger'] . '_show?id=' . $event['arranger_id']) ?><br />
-        <?php echo __('Categories') ?>: <?php foreach ($event['categories'] as $c) echo link_to($c['name'], $rp['category'] . '_show?id=' . $c['id']) . " "; ?><br />
-        <?php if ($event['festival_id'] > 0): ?> 
-          <?php echo __('Part of festival') ?>
-          <?php echo link_to($event['festival']['title'] . ' ' . format_date($event['festival']['startDate']), $rp['festival'] . '_show?id=' . $event['festival_id']) ?>
-        <?php endif ?>
+        <div class="shortEvent">
+          <?php echo link_to($event['title'], $rp['event'] . '_show?id=' . $event['id']) ?><br />
+          <span>
+            <?php
+            if (strlen(strip_tags($event->getRaw('leadParagraph'))) > 100) { 
+              echo substr(strip_tags($event->getRaw('leadParagraph')), 0, 97) . '...';
+            } else {
+              echo strip_tags($event->getRaw('leadParagraph'));
+            }
+            ?>
+          </span><br />
+          <?php echo __('When?') ?> <?php include_partial('dakEvent/startEndDateTime', array('event' => $event)) ?><br />
+          <?php echo __('Location') ?>: <?php include_partial('dakEvent/location', array('event' => $event)) ?><br />
+          <?php if (strlen($event['covercharge']) > 0): ?>
+            <b><?php echo __('Covercharge') ?></b>: <?php echo $event['covercharge'] ?><br />
+          <?php endif ?>
+          <?php echo __('Arranger') ?>: <?php echo link_to($event['arranger']['name'], $rp['arranger'] . '_show?id=' . $event['arranger_id']) ?><br />
+          <?php echo __('Categories') ?>: <?php foreach ($event['categories'] as $c) echo link_to($c['name'], $rp['category'] . '_show?id=' . $c['id']) . " "; ?><br />
+          <?php if ($event['festival_id'] > 0): ?> 
+            <?php echo __('Part of festival') ?>
+            <?php echo link_to($event['festival']['title'] . ' ' . format_date($event['festival']['startDate']), $rp['festival'] . '_show?id=' . $event['festival_id']) ?>
+          <?php endif ?>
+        </div>
       </td>
     </tr>
     <?php endforeach ?>
