f509956478e54a126268334727895a8f364b3345
Endre Oma <eoma@kvarteret.no>
2011-07-23 20:33:08 +0200

    Add proper icons to dakGlobal/_pager.php

--- modules/dakGlobal/templates/_pager.php
+++ modules/dakGlobal/templates/_pager.php
@@ -2,11 +2,11 @@
 
   <div class="pagination">
     <a href="<?php echo url_for($route) ?>?page=1">
-      <img src="<?php echo public_path('/images/first.png') ?>" alt="First page" title="First page" />
+      <img src="<?php echo public_path(sfConfig::get('dak_events_module_web_dir') . '/images/first.png') ?>" alt="First page" title="First page" />
     </a>
  
     <a href="<?php echo url_for($route) ?>?page=<?php echo $pager->getPreviousPage() ?>">
-      <img src="<?php echo public_path('/images/previous.png') ?>" alt="Previous page" title="Previous page" />
+      <img src="<?php echo public_path(sfConfig::get('dak_events_module_web_dir') . '/images/previous.png') ?>" alt="Previous page" title="Previous page" />
     </a>
  
     <?php foreach ($pager->getLinks() as $page): ?>
@@ -18,10 +18,10 @@
     <?php endforeach; ?>
  
     <a href="<?php echo url_for($route) ?>?page=<?php echo $pager->getNextPage() ?>">
-      <img src="<?php echo public_path('/images/next.png') ?>" alt="Next page" title="Next page" />
+      <img src="<?php echo public_path(sfConfig::get('dak_events_module_web_dir') . '/images/next.png') ?>" alt="Next page" title="Next page" />
     </a>
  
     <a href="<?php echo url_for($route) ?>?page=<?php echo $pager->getLastPage() ?>">
-      <img src="<?php echo public_path('/images/last.png') ?>" alt="Last page" title="Last page" />
+      <img src="<?php echo public_path(sfConfig::get('dak_events_module_web_dir') . '/images/last.png') ?>" alt="Last page" title="Last page" />
     </a>
   </div>
