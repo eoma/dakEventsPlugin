fb981d4ddc926fba4aeb96b98f12b7a190c2cb86
Endre Oma <eoma@kvarteret.no>
2011-10-26 12:29:47 +0200

    Add partial dakArrangerAdmin/logo

---
+++ modules/dakArrangerAdmin/templates/_logo.php
@@ -0,0 +1,4 @@
+<?php 
+if (!empty($dak_arranger['logo']['id']))
+include_partial('dakGlobal/picture', array('picture' => $dak_arranger['logo'], 'format' => 'listSmall', 'class' => 'eventPicture'));
+?>
